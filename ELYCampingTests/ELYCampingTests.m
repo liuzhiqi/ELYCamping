c23617b2b85225b7ab9a1e7443067d13bd89dbe3
Tristan <liuzhiqi@LiuZhiqideMac.local>
2015-01-08 07:54:45 -0800

    Initial Commit

---
+++ ELYCampingTests/ELYCampingTests.m
@@ -0,0 +1,40 @@
+//
+//  ELYCampingTests.m
+//  ELYCampingTests
+//
+//  Created by LiuZhiqi on 15-1-8.
+//  Copyright (c) 2015年 Eluying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ELYCampingTests : XCTestCase
+
+@end
+
+@implementation ELYCampingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
